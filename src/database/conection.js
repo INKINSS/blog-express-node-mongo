524e3b76487f3156ba4dea4605651c7f38f6b76c
samito <martinsamuelpp@gmail.com>
2024-06-19 20:07:30 -0600

    base de datos conectada al servidor

---
+++ src/database/conection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const conection = async () => {
+    try {
+        const username = process.env.VITE_USERNAME_DB;
+        const password = process.env.VITE_PASSWORD_DB;
+        const dbUrl = `mongodb+srv://${username}:${password}@blog.05ec6jh.mongodb.net/`;
+
+        await mongoose.connect(dbUrl)
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        throw new Error('Error connecting to MongoDB');
+    }
+};
+
+module.exports = conection;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express();
+const conection = require('./database/conection')
 
 const PORT = process.env.VITE_PORT || 3000;
+
+//conection
+conection()
+//user
+
+//set
 
 app.get('/', (req, res) => {
     res.send('Hello World');
