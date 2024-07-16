64a3bbfdca2b07082441969bf512ec8ee77864d1
samito <martinsamuelpp@gmail.com>
2024-07-16 11:27:33 -0600

    reestructura de modelo en details, user, content

--- src/models/blog.js
+++ src/models/blog.js
@@ -1,13 +1,24 @@
-const { Schema } = require('mongoose')
-const mongoose = require('mongoose')
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
 
-const postSchema = new Schema({
-    id: {
-        type: mongoose.Types.ObjectId,
-        auto: true,
+const userSchema = new Schema({
+    username: {
+        type: String,
         required: true,
-        unique: true
+        trim: true
     },
+    nickname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    publishDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const detailtSchema = new Schema({
     title: {
         type: String,
         required: true,
@@ -18,16 +29,19 @@ const postSchema = new Schema({
         required: true,
         trim: true
     },
-    tags: {
-        type: [String],
-        required: true,
-        trim: true
-    },
     introduction: {
         type: String,
         required: true,
         trim: true
     },
+    tags: {
+        type: [String],
+        required: true,
+        trim: true
+    },
+});
+
+const contentSchema = new Schema({
     title_content_1: {
         type: String,
         required: true,
@@ -48,13 +62,53 @@ const postSchema = new Schema({
         required: true,
         trim: true
     },
-    conclution: {
+    title_content_3: {
         type: String,
         required: true,
         trim: true
     },
-},
-)
+    content_3: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    title_content_4: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content_4: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    conclution: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+const postSchema = new Schema({
+    id: {
+        type: mongoose.Types.ObjectId,
+        auto: true,
+        required: true,
+        unique: true
+    },
+    user: {
+        type: userSchema,
+        required: true
+    },
+    detail: {
+        type: detailtSchema,
+        required: true
+    },
+    content: {
+        type: contentSchema,
+        required: true
+    }
+});
 
 postSchema.set('toJSON', {
     transform: (doc, ret) => {
@@ -62,7 +116,7 @@ postSchema.set('toJSON', {
         delete ret._id;
         delete ret.__v;
     }
-})
+});
 
 const Post = mongoose.model('Post', postSchema);
 
